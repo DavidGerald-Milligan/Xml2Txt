b63f9939e14c581f0bd5b0f15aec6fb9d0567315
David Milligan <davidgerald.milligan@microfocus.com>
2021-12-09 11:53:42 +0000

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Xml2Txt
+{
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            try
+            {
+                //  Read from console, how many files at what approximate size.
+                int pagesToCreate = 0;
+                int saveFilesize = 0;
+                Console.WriteLine("How many files?");
+                if (Int32.TryParse(Console.ReadLine(), out pagesToCreate))
+                {
+                    Console.WriteLine("Min Filesize in MB?");
+                    if (Int32.TryParse(Console.ReadLine(), out saveFilesize))
+                    {
+                        saveFilesize *= (1024 * 1024);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                // Keep track of running totals
+                int filesize = 0;
+                int pagesProcessed = 0;
+                int filesCreated = 0;
+
+                Console.WriteLine("Source file?");
+                StringBuilder sb = new StringBuilder("", 5000);
+                string path = Path.Combine(Environment.CurrentDirectory, Console.ReadLine());
+                XmlReader reader = XmlReader.Create(path);
+                reader.ReadToDescendant("page");
+                if (pagesToCreate > 0)
+                {
+                    Console.WriteLine("Enter the prepend for created files...");
+                    string outputFilePrepend = Console.ReadLine();
+                    if (!Directory.Exists(outputFilePrepend))
+                    {
+                        Directory.CreateDirectory(outputFilePrepend);
+                    }
+                    do
+                    {
+                        XmlDocument doc = new XmlDocument();
+                        doc.LoadXml(reader.ReadOuterXml());
+
+                        XmlNode pageNode = doc.DocumentElement;
+                        if (pageNode != null)
+                        {
+                            pagesProcessed++;
+                            foreach (XmlNode revisonNode in pageNode.ChildNodes)
+                            {
+                                if (revisonNode.Name == "revision")
+                                {
+                                    foreach (XmlNode textNode in revisonNode.ChildNodes)
+                                    {
+                                        if (textNode.Name == "text")
+                                        {
+                                            int thisFileSize = Int32.Parse(textNode.Attributes["bytes"].Value);
+                                            filesize += thisFileSize;
+                                            sb.Append(textNode.InnerText);
+                                            if (filesize >= saveFilesize)
+                                            {
+                                                string outputFile = outputFilePrepend + "_" + filesCreated++ + ".txt";
+                                                CreateFile(outputFilePrepend, outputFile, sb.ToString(), (filesize / 1024 / 1024));
+
+                                                sb = new StringBuilder("", 5000);
+                                                filesize = 0;
+                                                if (filesCreated >= pagesToCreate)
+                                                {
+                                                    goto LoopEnd;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    while (reader.ReadToNextSibling("page"));
+
+                    if (filesize > 0)
+                    {
+                        // if anything left write that to a file.
+                        string outputFile = outputFilePrepend + "_" + filesCreated++ + ".txt";
+                        CreateFile(outputFilePrepend, outputFile, sb.ToString(), (filesize / 1024 / 1024));
+                    }
+                }
+            LoopEnd:
+                reader.Close();
+                if (pagesProcessed == 0)
+                {
+                    Console.WriteLine("Nothing created, try again");
+                }
+                else
+                {
+                    Console.WriteLine("Processed:" + pagesProcessed);
+                    Console.WriteLine("Created:" + filesCreated);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Any key to quit");
+            Console.ReadLine();
+        }
+
+        static void CreateFile(string directory, string outputFile, string content, int size)
+        {
+            using (FileStream fs = File.Create(directory + "/" + outputFile))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(content);
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+
+            Console.WriteLine("Created:" + outputFile + " Size:" + size + "MB");
+        }
+    }
+ }
+
+
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Xml2Txt
+
+##  The Xml2Txt application reads from a wikipedia dump file, and outputs a defined number of files at a defined minimum size.
+
+For simlicity the application has already been build and stored in the ./publish directory.  
+The ./publish directory contains a small sample data.xml file of 10MB in size to get you moving.
+
+###  Download a dump file 
+.xml.bz2 compressed files can be downloaded from https://dumps.wikimedia.org/enwiki/
+
+Example:/20210901/enwiki-20210901-pages-articles-multistream.xml.bz2
+
+This is an 18GB compressed file containing  some 25,000,0000 wikipedia pages.
+
+Use a utility such as [7-zip](https://www.7-zip.org/download.html) to extract this to enwiki-20210901-pages-articles-multistream.xml,
+
+Rename the extracted file to something simpler, like data.xml.
+
+The extracted file is now some 90GB.
+
+Copy the ../publish/Xml2Txt.exe to the same directory as the .xml file.
+
+Click on Xml2Txt.exe and you will be prompted for:
+- The number of files to create.
+- The minimum size of each file in MB.
+- The data file to use (maybe data.xml).
+- A directory to add files to.
+
+The application will read each page from the xml, when it has read content greater than the minimum file size it will create a file in the defined directory.
+
+Example:
+5 files, min size 10 MB, using data.xml as input, writing to directory "BIG" will produce:
+- BIG
+	- BIG_1.txt
+	- BIG_2.txt
+	- BIG_3.txt
+	- BIG_4.txt
+	- BIG_5.txt
+
